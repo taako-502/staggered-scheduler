b2400ab6df11b3107efef26b5bcc41af1dd4b73a
taako-502 <i.am.armorknight@gmail.com>
2023-11-11 05:51:13 +0200

    余計なタグを削除

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -3,7 +3,6 @@ import Login from '@/components/Login'
 const Signin = () => {
   return (
     <div>
-      <h1>Signin</h1>
       <Login className="block max-w-screen-sm	mx-auto px-2 py-3 border-2 border-slate-300	border-solid" />
     </div>
   )
