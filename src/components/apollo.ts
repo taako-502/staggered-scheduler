fdb2d2a3279cf2c676c596e6b97fff15212dd946
taako-502 <i.am.armorknight@gmail.com>
2023-12-07 19:47:07 +0200

    不要ファイルを削除

--- src/components/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const apolloClient = new ApolloClient({
-  uri: 'http://localhost:8080/query',
-  cache: new InMemoryCache(),
-})
